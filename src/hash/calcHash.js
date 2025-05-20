7c0447f1f33233f47cb6c1df8cc1604d60a35c82
nastyahh <nasstyahus@gmail.com>
2025-05-20 20:23:37 +0300

    feat: add hash calculation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    try {
+        const fileContent = await readFile(filePath);
+        const hash = createHash('sha256').update(fileContent).digest('hex');
+        console.log(hash);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+        throw error;
+    }
 };
 
 await calculateHash();
