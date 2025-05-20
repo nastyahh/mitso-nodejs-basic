9e6e253eddea4d7881d8705579170d6baf832123
nastyahh <nasstyahus@gmail.com>
2025-05-20 19:54:34 +0300

    create read func

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import { readFile, access } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        await access(filePath);
+        const content = await readFile(filePath, 'utf-8');
+        console.log(content);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+        throw error;
+    }
 };
 
 await read();
