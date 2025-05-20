a47553b27b3777e0691f370246d59e3a4aad8185
nastyahh <nasstyahus@gmail.com>
2025-05-20 19:38:38 +0300

    create copy func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import { cp, access } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const copy = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const sourceDir = join(__dirname, 'files');
+    const targetDir = join(__dirname, 'files_copy');
+
+    try {
+        await access(sourceDir);
+
+        await cp(sourceDir, targetDir, {
+            recursive: true,
+            force: false
+        });
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+        if (error.code === 'EEXIST') {
+            throw new Error('FS operation failed');
+        }
+        throw error;
+    }
 };
 
 await copy();
