6e8240344942d625c5da12a33ce06b9f5e02fd39
nastyahh <nasstyahus@gmail.com>
2025-05-20 20:32:23 +0300

    create streams write func

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,33 @@
+import { createWriteStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+
+    try {
+        const writeStream = createWriteStream(filePath, { flags: 'w' });
+
+        process.stdin.setEncoding('utf-8');
+
+        process.stdin.on('data', (chunk) => {
+            writeStream.write(chunk);
+        });
+
+        process.stdin.on('end', () => {
+            writeStream.end();
+        });
+
+        writeStream.on('error', () => {
+            throw new Error('FS operation failed');
+        });
+
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await write();
