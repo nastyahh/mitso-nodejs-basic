78441ee44a0650cf9694492f7857d2c19735afba
nastyahh <nasstyahus@gmail.com>
2025-05-20 20:13:10 +0300

    create parseEnv func

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const mitsoVars = Object.entries(process.env)
+        .filter(([key]) => key.startsWith('MITSO_'))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+
+    console.log(mitsoVars);
 };
 
 parseEnv();
