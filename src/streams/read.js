05fc314e262d4e05281f1e45e52761f641c13013
nastyahh <nasstyahus@gmail.com>
2025-05-20 20:26:27 +0300

    add stream file reading

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        const readStream = createReadStream(filePath);
+        readStream.pipe(process.stdout);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+        throw error;
+    }
 };
 
 await read();
