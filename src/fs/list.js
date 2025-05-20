4ee9711cdd328a4fafb7808d46bc2de5d6eb4f91
nastyahh <nasstyahus@gmail.com>
2025-05-20 19:53:08 +0300

    create list func

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import { readdir, access } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const list = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const dirPath = join(__dirname, 'files');
+
+    try {
+        await access(dirPath);
+        const files = await readdir(dirPath);
+        console.log(files);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+        throw error;
+    }
 };
 
 await list();
