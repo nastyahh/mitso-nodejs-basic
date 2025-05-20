a9df94baaa5808e118d124053bbc738b4d6f2a69
nastyahh <nasstyahus@gmail.com>
2025-05-20 19:50:14 +0300

    create remove func

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import { unlink, access } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const remove = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        await access(filePath);
+        await unlink(filePath);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+        throw error;
+    }
 };
 
 await remove();
